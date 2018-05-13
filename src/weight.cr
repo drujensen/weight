86b293203a770ddf60151d806b2712185a683b77
Dru Jensen <drujensen@gmail.com>
2018-05-13 13:04:43 -0700

    add denormalize

--- src/weight.cr
+++ src/weight.cr
@@ -17,18 +17,26 @@ end
 
 # normalize the data
 training = SHAInet::TrainingData.new(inputs, outputs)
+training.normalize_min_max
 
 # create a network
 model : SHAInet::Network = SHAInet::Network.new
 model.add_layer(:input, 1, :memory, SHAInet.none)
-model.add_layer(:hidden, 1, :memory, SHAInet.none)
+model.add_layer(:hidden, 1, :memory, SHAInet.sigmoid)
 model.add_layer(:output, 1, :memory, SHAInet.none)
 model.fully_connect
 
 # train the network
-model.train(training.raw_data, :sgdm, :mse, 10000, -1.0)
+model.train(training.data, :sgdm, :mse, 10000, -1.0)
 
-model.show
+results = model.run(training.normalize_inputs([1.47]))
+puts training.denormalize_outputs(results)
 
-model.run([1.47])
-model.run([1.83])
+results = model.run(training.normalize_inputs([1.65]))
+puts training.denormalize_outputs(results)
+
+results = model.run(training.normalize_inputs([1.83]))
+puts training.denormalize_outputs(results)
+
+results = model.run(training.normalize_inputs([2.0]))
+puts training.denormalize_outputs(results)
