9bbac4ba9c4004d01ce075e9d86cd2ef2f2c8c0d
Dru Jensen <drujensen@gmail.com>
2021-11-17 21:28:36 -0800

    feat: replace shainet with evolvenet

--- src/weight.cr
+++ src/weight.cr
@@ -1,5 +1,5 @@
 require "csv"
-require "shainet"
+require "evolvenet"
 
 # data structures to hold the input and results
 inputs = Array(Array(Float64)).new
@@ -16,26 +16,26 @@ while (csv.next)
 end
 
 # normalize the data
-training = SHAInet::TrainingData.new(inputs, outputs)
+training = EvolveNet::TrainingData.new(inputs, outputs)
 training.normalize_min_max
 
 # create a network
-model : SHAInet::Network = SHAInet::Network.new
-model.add_layer(:input, 1, :memory, SHAInet.none)
-model.add_layer(:hidden, 1, :memory, SHAInet.sigmoid)
-model.add_layer(:output, 1, :memory, SHAInet.none)
+model : EvolveNet::Network = EvolveNet::Network.new
+model.add_layer(:input, 1, :none)
+model.add_layer(:hidden, 1, :sigmoid)
+model.add_layer(:output, 1, :none)
 model.fully_connect
 
-# train the network
-model.learning_rate = 0.01
-model.train(training.data, :sgdm, :mse, 200, -1.0, 10)
+# evolve the network
+organism = EvolveNet::Organism.new(model, 10, 10)
+model = organism.evolve(training.data, 1000)
 
 results = Array(Float64).new
-results << model.run([0])[0].round(8)
-results << model.run([0.25])[0].round(8)
-results << model.run([0.5])[0].round(8)
-results << model.run([0.75])[0].round(8)
-results << model.run([1])[0].round(8)
+results << model.run([0])[0].to_f64.round(8)
+results << model.run([0.25])[0].to_f64.round(8)
+results << model.run([0.5])[0].to_f64.round(8)
+results << model.run([0.75])[0].to_f64.round(8)
+results << model.run([1])[0].to_f64.round(8)
 
 puts results
 # results = model.run(training.normalize_inputs([75]))
